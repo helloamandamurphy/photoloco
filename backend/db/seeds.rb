2c9e3b31e2e1317ca642894b901235ce340e0983
Amanda Murphy <helloamandamurphy@gmail.com>
2019-11-13 12:46:15 -0500

    Create seed data, confirm seed data exists in console and server--it does

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Location.create([
+  { name: 'Test #1', lat: '111.111111', long: '-111.111111'},
+  { name: 'Test #2', lat: '222.222222', long: '-222.222222'},
+  { name: 'Test #3', lat: '333.333333', long: '-333.333333'},
+  { name: 'Test #4', lat: '444.444444', long: '-444.444444'},
+  { name: 'Test #5', lat: '555.555555', long: '-555.555555'}
+  ])
