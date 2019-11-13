c982f98b6cc9290807b5783f491a0f9ed608403f
Amanda Murphy <helloamandamurphy@gmail.com>
2019-11-13 12:39:24 -0500

    Create LocationsController and write basic CRUD functions

---
+++ backend/app/controllers/api/v1/locations_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+    render json: @locations, status: 200
+  end
+
+  def show
+    @location = Location.find(params[:id])
+    render json: @location, status: 200
+  end
+
+  def create
+    @location = Location.create(location_params)
+    render json: @location, status: 200
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    @location.update(location_params)
+    render json: @location, status: 200
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.delete
+
+    render json: {locationId: @location.id}
+  end
+
+
+  private
+    def location_params
+      params.require(:location).permit(:name, :lat, :long)
+    end
+end
