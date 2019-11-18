21691fe642e01a4e2bb3a9a0dda8a06dc468356b
Amanda Murphy <helloamandamurphy@gmail.com>
2019-11-18 10:16:11 -0500

    Add namespace routes for tags and photos

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :locations
+      resources :tags, only: [:index, :create, :new, :show]
+      resources :photos
     end
   end
 end
