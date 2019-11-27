755422d605d37c2d7e1ef762f8060b34cdb8e75f
Amanda Murphy <helloamandamurphy@gmail.com>
2019-11-27 13:18:33 -0500

    Add like button and like count to CSS/HTML--still need to add EventListener to JS side of things to update like count

--- frontend/src/components/location.js
+++ frontend/src/components/location.js
@@ -14,13 +14,16 @@ class Location {
       <img class="card-img" src="${this.photo}" alt="${this.name}">
       <h2 class="card-title">${this.name}</h2>
       <div class="card-content">
-        <p class="likes">Likes: ${this.likes}</p>
         <ul>
           <li>Tag</li>
           <li>Tag</li>
           <li>Tag</li>
         </ul>
         <a class="card-link" target=”_blank” href="http://www.google.com/maps/place/${this.lat},${this.long}">Find this Location</a>
+        <button type='button'>
+          ${this.likes} Likes
+          <span><i class="fas fa-thumbs-up"></i></span>
+        </button>
       </div>
     </div>
     `
